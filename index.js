7d9a1199c3e9ccd4fbb0eb15fbf3c22b17875498
Darío Rojas <dariomrojas89@gmail.com>
2022-09-08 21:44:48 -0300

    Primer entrega

---
+++ index.js
@@ -0,0 +1,10 @@
+let entrada = prompt ("Qué día es hoy?")
+
+while(entrada != "lunes"){
+    let mensaje = `La semana ya empezó`
+    alert(mensaje)
+
+    let entrada = prompt ("Qué día es hoy?")
+}
+
+alert ("Buen comienzo de semana")
