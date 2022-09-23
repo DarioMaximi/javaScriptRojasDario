1028faf61da13635e74f7b234226cc6fa55210bc
Darío Rojas <dariomrojas89@gmail.com>
2022-09-22 22:35:34 -0300

    mejorando el proyecto

--- index.js
+++ index.js
@@ -5,17 +5,35 @@ function solicitarNombre(){
 
 solicitarNombre();
 
-let Moneda = prompt("El valor de que moneda desea saber?")
+class Moneda{
+  constructor(nombre, precioDeCompra, precioDeVenta){
+  this.nombre = nombre;
+  this.precioDeCompra = precioDeCompra;
+  this.precioDeVenta = precioDeVenta;
+}
+}
 
-switch (Moneda) {
-    case 'dolar':
-      alert ('El dolar cotiza hoy a $295.-');
+const Moneda1 = new Moneda("dolar", 275, 290);
+const Moneda2 = new Moneda("euro", 285, 305);
+const Moneda3 = new Moneda("real", 40, 60);
+
+let cambio = prompt("El valor de que moneda desea saber?")
+
+switch (cambio) {
+    case Moneda1.nombre :
+      alert ('Precio de compra: ' + Moneda1.precioDeCompra + ' Precio de venta: ' + Moneda1.precioDeVenta );
+      let cantidad = prompt('Que cantidad queres cambiar?');
+      alert ('Cambiando ' + cantidad + ' dolares, vas a obtener ' + cantidad * Moneda1.precioDeCompra +' pesos. Cambiando ' + cantidad +' pesos, vas a obtener ' + cantidad / Moneda1.precioDeVenta + ' dolares.');
       break;
-    case 'euro':
-      alert ('El euro cotiza hoy a $300');
+    case Moneda2.nombre:
+      alert ('Precio de compra: ' + Moneda2.precioDeCompra + ' Precio de venta: ' + Moneda2.precioDeVenta );
+      let cantidad1 = prompt('Que cantidad queres cambiar?');
+      alert ('Cambiando ' + cantidad1 + ' euros, vas a obtener ' + cantidad1 * Moneda2.precioDeCompra +' pesos. Cambiando ' + cantidad1 +' pesos, vas a obtener ' + cantidad1 / Moneda2.precioDeVenta + ' euros.');
       break;
-    case 'real':
-      alert ('El real cotiza hoy a $30');
+    case Moneda3.nombre:
+      alert ('Precio de compra: ' + Moneda3.precioDeCompra + ' Precio de venta: ' + Moneda3.precioDeVenta );
+      let cantidad2 = prompt('Que cantidad queres cambiar?');
+      alert ('Cambiando ' + cantidad2 + ' reales, vas a obtener ' + cantidad2 * Moneda3.precioDeCompra +' pesos. Cambiando ' + cantidad2 +' pesos, vas a obtener ' + cantidad2 / Moneda3.precioDeVenta + ' reales.');
       break;
     default:
       alert ('Lo lamentamos, por el momento no disponemos de la cotizacion de ' + Moneda + '.');
@@ -24,6 +42,7 @@ switch (Moneda) {
   alert ("¿Hay algún cambio más que quisiera hacer?");
 
   /* incorporando array  */
+  
 
   let monedas = ["dolar", "euro", "real"]
 
