82158206dc1134a4a004afa969702ddeb8e2f24c
Darío Rojas <dariomrojas89@gmail.com>
2022-11-14 12:20:19 -0300

    arreglo del trabajo, utilizando fetch y storage

--- index.js
+++ index.js
@@ -1,93 +1,108 @@
-let boton = document.getElementById("boton")
-boton.classList.add ("colorTamaño")
+const inputs = document.querySelectorAll(".valor");
+let name = prompt("Bienvenido, ingrese su nombre por favor")
 
-boton.addEventListener("click", () =>{
-  Swal.fire({
-    title: 'Estas seguro que quieres cambiar divisas?',
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Si, cambiar!'
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire(
-        hacerCambio()
-      )
-    }
-  })
-});
+const traerCambios = async () => {
+    const response = await fetch("./data.json");
+    const data = await response.json();
 
-function ingresarNombre(){
-    let nombreIngresado   = prompt("Ingresar nombre")
-    alert("Bienvenid@ " + nombreIngresado + " a la casa de cambio D.R.")
+    document.querySelector('#USD')
+      .dataset.cambio = data[1].valor;
+    document.querySelector('#EUR')
+      .dataset.cambio = data[2].valor;
+    document.querySelector('#BRA')
+      .dataset.cambio = data[3].valor;
+    document.querySelector('#COL')
+      .dataset.cambio = data[4].valor;
+
+    inputs.forEach(input => {
+        input.value = input.dataset.cambio;
+    });
+
 }
 
-ingresarNombre()
-class Moneda{
-  constructor(info){
-  this.nombre = info.nombre;
-  this.precioDeCompra = info.precioDeCompra;
-  this.precioDeVenta = info.precioDeVenta;
-  this.imagen = info.imagen;
-}
-}
-const moneda = [];
+traerCambios()
 
-moneda.push (new Moneda({nombre: "dolar", precioDeCompra: 275, precioDeVenta: 290, imagen: "./images/dolar.png"}));
-moneda.push (new Moneda({nombre: "euro",precioDeCompra: 285, precioDeVenta: 305, imagen: "./images/euro.png"}));
-moneda.push (new Moneda({nombre: "real",precioDeCompra: 40, precioDeVenta: 60, imagen: "./images/real.png"}));
+function valorCambiado(input) {
+    let factor = input.value / input.dataset.cambio;
+    inputs.forEach(campo => {
+      campo.value = (campo.dataset.cambio * factor).toFixed(2);
+    })
+  }
 
-function hacerCambio(){
-    let cambio = prompt("El valor de que moneda desea saber?").toLowerCase()
+const button = document.createElement('button')
+
+button.innerText = 'Cotización del dolar'
+
+button.addEventListener('click', function () {
+  Swal.fire(
+    'Cotizaciones del día',
+    'Dolar: 100 USD = ' + monedas[1].precio + ' ARG ',
+    'info'
+  )
+} )
+
+document.body.append (button)
+
+const button1 = document.createElement('button')
+
+button1.innerText = 'Cotización del euro'
+
+button1.addEventListener('click', function () {
+  Swal.fire(
+    'Cotizaciones del día',
+    'Euro: 100 EUR = ' + monedas[2].precio + 'ARG',
+    'info'
+  )
+} )
+
+document.body.append (button1)
+
+const button2 = document.createElement('button')
+
+button2.innerText = 'Cotización del real'
+
+button2.addEventListener('click', function () {
+  Swal.fire(
+    'Cotizaciones del día',
+    'Real: compra: 100 BRA = ' + monedas[3].precio + ' ARG',
+    'info'
+  )
+} )
+
+document.body.append (button2)
+
+const button3 = document.createElement('button')
+
+button3.innerText = 'Cotización del peso colombiano'
+
+button3.addEventListener('click', function () {
+  Swal.fire(
+    'Cotizaciones del día',
+    'Real: compra: 100 COL = ' + monedas[4].precio + ' ARG',
+    'info'
+)
+} )
+
+document.body.append (button3)
+
+const monedas = [
+  { id: 1, nombre: "Pesos Argentinos", precio: 1 },
+  { id: 2, nombre: "Dólares", precio: 16129.03 },
+  { id: 3, nombre: "Euros", precio: 16393.44 },
+  { id: 4, nombre: "Reales", precio: 3030.3 },
+  { id: 4, nombre: "Pesos Colombianos", precio: 3.36 },
+];
+localStorage.setItem("bolsa", JSON.stringify(monedas));
+
+const cambiarValor = (nombre, precio) => {
+  let bolsa = JSON.parse(localStorage.getItem("bolsa"));
+  let monedas = bolsa.find(item => item.nombre === nombre);
+  monedas.precio = precio;
+  localStorage.setItem("bolsa", JSON.stringify(bolsa));
+};
+
+let nombre = prompt("Ingrese el nombre de la moneda");
+let precio = prompt("Ingrese el monto en pesos argentinos que se necesitan para adquirir 100 unidades");
+cambiarValor(nombre, precio);
 
 
-switch (cambio) {
-    case (moneda[0].nombre):
-      alert ('Precio de compra: ' + moneda[0].precioDeCompra + ' Precio de venta: ' + moneda[0].precioDeVenta );
-      let cantidad = prompt('Que cantidad queres cambiar?');
-      alert ('Cambiando ' + cantidad + ' dolares, vas a obtener ' + cantidad * moneda[0].precioDeCompra +' pesos. Cambiando ' + cantidad +' pesos, vas a obtener ' + cantidad / moneda[0].precioDeVenta + ' dolares.');
-      break;
-    case (moneda[1].nombre):
-      alert ('Precio de compra: ' + moneda[1].precioDeCompra + ' Precio de venta: ' + moneda[1].precioDeVenta );
-      let cantidad1 = prompt('Que cantidad queres cambiar?');
-      alert ('Cambiando ' + cantidad1 + ' euros, vas a obtener ' + cantidad1 * moneda[1].precioDeCompra +' pesos. Cambiando ' + cantidad1 +' pesos, vas a obtener ' + cantidad1 / moneda[1].precioDeVenta + ' euros.');
-      break;
-    case (moneda[2].nombre):
-      alert ('Precio de compra: ' + moneda[2].precioDeCompra + ' Precio de venta: ' + moneda[2].precioDeVenta );
-      let cantidad2 = prompt('Que cantidad queres cambiar?');
-      alert ('Cambiando ' + cantidad2 + ' reales, vas a obtener ' + cantidad2 * moneda[2].precioDeCompra +' pesos. Cambiando ' + cantidad2 +' pesos, vas a obtener ' + cantidad2 / moneda[2].precioDeVenta + ' reales.');
-      break;
-    default:
-      alert ('Lo lamentamos, por el momento no disponemos de la cotizacion de ' + cambio + '.');
-      }
-    }
-  
-  
-
-  /* incorporando array  */
-  
-
-  let monedas = ["dolar", "euro", "real"]
-
-  console.log(monedas.length);
-
-  /* llamando productos creados para mostrar en el inicio */
-
-  let imagenesInicio = document.getElementById("imagenesInicio")
-
-  moneda.forEach((moneda) => {
-    let productoCarrousel = document.createElement("div")
-    productoCarrousel.innerHTML = `
-    <div class="card" id="tarjertas" style="width: 18rem;">
-  <img class="card-img-top" id="tarjetaProducto" src="${moneda.imagen}" alt="Card image cap">
-  <div class="card-body">
-    <h2 class="card-title">${moneda.nombre}</h2>
-    <p class="card-text">"Precio de Compra: ${moneda.precioDeCompra}"</p>
-    <p class="card-text">"Precio de Venta: ${moneda.precioDeVenta}"</p>
-  </div>
-</div>
-   `
-imagenesInicio.append(productoCarrousel)
-
-})
